39890cdbfba5826e25ec40bbfd34e2c0d2a46734
Bavan <138361697+bavanchun@users.noreply.github.com>
2024-09-26 21:46:19 +0700

    handle register

--- Front-end/koi-fish/src/main.jsx
+++ Front-end/koi-fish/src/main.jsx
@@ -2,5 +2,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <ToastContainer />
+  </>
+);
