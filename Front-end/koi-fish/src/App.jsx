e06cc33983c79446d8f1303370be9978f6546f70
Bavan <138361697+bavanchun@users.noreply.github.com>
2024-09-26 22:27:47 +0700

    create template dashboard

--- Front-end/koi-fish/src/App.jsx
+++ Front-end/koi-fish/src/App.jsx
@@ -3,20 +3,25 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./page/login/login";
 import RegisterPage from "./page/register/register";
+import Dashboard from "./components/dashboard/dashboard";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "",
       element: <div>Hello world!</div>,
     },
     {
-      path: "/login",
+      path: "login",
       element: <LoginPage />,
     },
     {
-      path: "/register",
+      path: "register",
       element: <RegisterPage />,
+    },
+    {
+      path: "dashboard",
+      element: <Dashboard />,
     },
   ]);
 
