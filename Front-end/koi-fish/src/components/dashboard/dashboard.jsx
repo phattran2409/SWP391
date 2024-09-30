45129f6ae57491d8114949e2ca23317dfab23794
Bavan <138361697+bavanchun@users.noreply.github.com>
2024-09-30 20:09:29 +0700

    fix manageMemmber and managePond

--- Front-end/koi-fish/src/components/dashboard/dashboard.jsx
+++ Front-end/koi-fish/src/components/dashboard/dashboard.jsx
@@ -19,17 +19,9 @@ function getItem(label, key, icon, children) {
 }
 const items = [
   getItem("Manage Member", "member", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
+  getItem("Manage Pond", "pond", <PieChartOutlined />),
+  getItem("Manage Koi", "koi", <PieChartOutlined />),
+  getItem("Manage Service", "service", <PieChartOutlined />),
 ];
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
