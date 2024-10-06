ec55241b377bd288225c5c0c40fb9e7417f34992
Bavan <138361697+bavanchun@users.noreply.github.com>
2024-10-06 18:01:41 +0700

    create managePost

--- Front-end/koi-fish/src/App.jsx
+++ Front-end/koi-fish/src/App.jsx
@@ -5,9 +5,12 @@ import LoginPage from "./page/login/login";
 import RegisterPage from "./page/register/register";
 import Dashboard from "./components/dashboard/dashboard";
 import ManageKoiFish from "./page/admin/manage-koi/manageKoiFish";
-import ManageService from "./page/admin/manage-service/manageService";
+// import ManageService from "./page/admin/manage-service/manageService";
 import ManageMembers from "./page/admin/manage-member/manageMembers";
 import ManagePonds from "./page/admin/manage-pond/managePonds";
+import ManageNews from "./page/admin/manage-post/manage-news/manageNews";
+import ManageBlogs from "./page/admin/manage-post/manage-blogs/manageBlogs";
+import ManageAds from "./page/admin/manage-post/manage-ads/manageAds";
 
 function App() {
   const router = createBrowserRouter([
@@ -40,8 +43,21 @@ function App() {
           element: <ManageKoiFish />,
         },
         {
-          path: "service",
-          element: <ManageService />,
+          path: "post",
+          children: [
+            {
+              path: "news",
+              element: <ManageNews />,
+            },
+            {
+              path: "blog",
+              element: <ManageBlogs />,
+            },
+            {
+              path: "ads",
+              element: <ManageAds />,
+            },
+          ],
         },
       ],
     },
